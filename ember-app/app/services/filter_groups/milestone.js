0918175a1b6bd4c430d364feefcc7a03fc62f527
Ryan Rauh <rauh.ryan@gmail.com>
2015-08-20 18:45:26 -0500

    Forces the milestone filters to remember the previous mode setting.
    YUCKY but it works

--- ember-app/app/services/filter_groups/milestone.js
+++ ember-app/app/services/filter_groups/milestone.js
@@ -5,14 +5,22 @@ function attr(modelProp, map) {
     get: function(key){
       var filters = this.get("model." + modelProp).map(map);
       filters.insertAt(0, Ember.Object.create({
-      name: 'No milestone',
-      queryParam: "milestone",
-      mode:0,
-      condition:function(i){
-        return i.milestone == null;
+        name: 'No milestone',
+        queryParam: "milestone",
+        mode:0,
+        condition:function(i){
+          return i.milestone == null;
+        }
+      }));
+      if(this._filters){
+        this._filters.forEach(function(f){
+          var newOne = filters.findBy('name', f.name);
+          if(newOne){
+            newOne.set('mode', f.mode);
+          }
+        });
       }
-    }));
-
+      this._filters = filters;
       return filters;
     },
     set: function(key, value){
@@ -28,14 +36,14 @@ var MilestoneFilters = Ember.Service.extend({
 
   create: function(model){
     this.set("filters", attr("filterMilestones", function(m){
-       return Ember.Object.create({
+      return Ember.Object.create({
         name: m.title,
         queryParam: "milestone",
         mode:0,
         condition:function(i){
-         return i.milestone && i.milestone.title.toLocaleLowerCase() === m.title.toLocaleLowerCase();
+          return i.milestone && i.milestone.title.toLocaleLowerCase() === m.title.toLocaleLowerCase();
         }
-       });
+      });
     }));
     return this.get("filters");
   }
