95c20ecb060d69f0eb5db28590ddfea13faac6aa
Richard Frank <richardfrank@me.com>
2015-09-08 16:21:00 -0600

    Adds update labels, add computed property for issue api calls to DRY things up

--- ember-app/app/models/model.js
+++ ember-app/app/models/model.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 import ajax from 'ic-ajax';
 import correlationId from 'app/utilities/correlation-id';
+import Serializable from 'app/mixins/serializable';
 
-var HuBoardModel = Ember.Object.extend(Ember._ProxyMixin,{
+var HuBoardModel = Ember.Object.extend(
+  Ember._ProxyMixin, Serializable, {
   correlationId: correlationId,
   _onInit: function(){
     this.set('content', this.get('data'));
--- ember-app/app/models/new/issue.js
+++ ember-app/app/models/new/issue.js
@@ -9,52 +9,37 @@ var Issue = Model.extend({
   milestoneTitle: Ember.computed.alias("milestone.title"),
   correlationId: correlationId,
   assignee: Ember.computed.alias("data.assignee"),
+  apiUrl: function(){
+    var full_name = this.get("repo.data.repo.full_name");
+    return `/api/${full_name}/issues/${this.get("data.number")}`;
+  }.property("data.number", "repo.data.repo.full_name"),
 
   loadDetails: function () {
     this.set("processing", true);
-    var user = this.get("data.repo.owner.login"),
-    repo = this.get("data.repo.name"),
-    full_name = user + "/" + repo;
-
-    return Ember.$.getJSON("/api/" + full_name + "/issues/" + this.get("data.number") + "/details")
+    return Ember.$.getJSON(`${this.get("apiUrl")}/details`)
     .success(function(details){
       this.set("data.repo", details.repo);
-      this.set("activities", details.activities);
+      this.set("data.activities", details.activities);
       this.set("processing", false);
     }.bind(this)).fail(function(){
       this.set("processing", false);
     }.bind(this));
   },
-  customState: Ember.computed("_data.custom_state", {
-    get:function(){
-      return this.get("_data.custom_state");
-    },
-    set: function (key, value) {
-      var full_name = this.get("repo.data.repo.full_name");
-      var previousState = this.get("_data.custom_state")
-
-      this.set("_data.custom_state", value);
-      this.set("processing", true);
-
-      var endpoint = value === "" ? previousState : value;
-      var number = this.get("data.number");
-      var options = {
-        dataType: "json",
-        data: {correlationId: this.get("correlationId")},
-        type: value === "" ? "DELETE" : "PUT",
-        url: `/api/${full_name}/issues/${number}/${endpoint}`
-      };
-
-      Ember.$.ajax(options)
-      .then(function(response){
+  updateLabels : function () {
+    this.set("processing", true);
+    return Ember.$.ajax( {
+      url: `${this.get("apiUrl")}`,
+      data: JSON.stringify({
+        labels: this.serialize(["repo"]).data.other_labels,
+        correlationId: this.get("correlationId")
+      }),
+      dataType: 'json',
+      type: "PUT",
+      contentType: "application/json"})
+      .then(function(){
         this.set("processing", false);
-        this.set("data.body", response.body);
-        this.set("data.body_html", response.body_html);
       }.bind(this));
-
-      return value;
-    },
-  }),
+  },
   reorder: function (index, column) {
     var changedColumns = this.get("data.current_state.index") !== column.data.index;
     if(changedColumns){ this.set("data._data.custom_state", ""); }
@@ -74,7 +59,35 @@ var Issue = Model.extend({
       this.set("data.body_html", response.body_html);
       return this;
     }.bind(this), "json");
-  }
+  },
+  customState: Ember.computed("_data.custom_state", {
+    get:function(){
+      return this.get("_data.custom_state");
+    },
+    set: function (key, value) {
+      var previousState = this.get("_data.custom_state")
+      this.set("_data.custom_state", value);
+
+      var endpoint = value === "" ? previousState : value;
+      var number = this.get("data.number");
+      var options = {
+        dataType: "json",
+        data: {correlationId: this.get("correlationId")},
+        type: value === "" ? "DELETE" : "PUT",
+        url: `${this.get("apiUrl")}/${endpoint}`
+      };
+
+      this.set("processing", true);
+      Ember.$.ajax(options)
+      .then(function(response){
+        this.set("processing", false);
+        this.set("data.body", response.body);
+        this.set("data.body_html", response.body_html);
+      }.bind(this));
+
+      return value;
+    },
+  }),
 });
 
 export default Issue;
