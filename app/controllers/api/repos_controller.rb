de4b798282ef912fdf321d66ce897ba443539e0e
Ryan Rauh <rauh.ryan@gmail.com>
2015-09-15 11:27:10 -0500

    deletes old board loading routes, slims loading by one ajax call
     * all repos now have boards
     * all boards now have repo
     * links are link repos with boards

--- app/controllers/api/repos_controller.rb
+++ app/controllers/api/repos_controller.rb
@@ -6,12 +6,5 @@ module Api
     def details
       render json: { data: huboard.repo(params[:user], params[:repo]).details }
     end
-    def issues
-      render json: { data: huboard.repo(params[:user], params[:repo]).fetch_issues }
-    end
-    def board
-      response = huboard.board(params[:user], params[:repo]).fetch
-      render json: {data: response}
-    end
   end
 end
