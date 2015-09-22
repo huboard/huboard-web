5dae291770123435595bfea431e57598c79e6d04
Ryan Rauh <rauh.ryan@gmail.com>
2015-09-22 10:35:39 -0500

    adds styling to the health checks module

--- ember-app/app/controllers/settings/health.js
+++ ember-app/app/controllers/settings/health.js
@@ -3,8 +3,21 @@ import ajax from 'ic-ajax';
 
 var SettingsHealthController = Ember.Controller.extend({
   isProcessing: false,
+  checks: Ember.computed.alias('model.health.content.checks'),
+  errorCount: Ember.computed('checks.@each.success', {
+    get: function(key) {
+      return this.get('checks').filter((x) => !Ember.get(x, 'success')).length;
+    }
+  }),
+  successCount: Ember.computed('checks.@each.success', {
+    get: function(key) {
+      return this.get('checks').filter((x) => Ember.get(x, 'success')).length;
+    }
+  }),
   actions: {
     treat(check, repo) {
+      Ember.set(check, 'success', true);
+      return;
       const controller = this;
       this.set('isProcessing', true);
       return ajax({
