da61378eec73e8a2f8e5bf6291d67065b297d3ec
Richard Frank <richardfrank@me.com>
2015-10-28 21:22:08 -0400

    Handles commit references from external repos gracefully

--- ember-app/app/controllers/issue/reference.js
+++ ember-app/app/controllers/issue/reference.js
@@ -5,17 +5,27 @@ var IssueReferenceController = Ember.Controller.extend({
   needs: ["issue"],
 
   fetchCommit: function(commit){
+    var _self = this;
     var repo = this.get("controllers.issue.model.repo.data.repo.full_name");
-    return ajax("/api/" + repo + "/commit/" + commit)
+    return ajax("/api/" + repo + "/commit/" + commit.commit_id)
       .then(function(response){
         if(response.message === "Not Found"){
           return {
-            sha: commit,
-            commit: {message: "Commit referenced from another repo"}
+            sha: commit.commit_id,
+            html_url: _self.toHtml(commit.commit_url),
+            commit: {
+              message: "(currently unable to fetch this commit message)"
+            }
           }
         }
         return response;
       });
+  },
+  //It's silly, but need this to call the html versus the api endpoint
+  toHtml: function(url){
+    return url.replace("api.", "")
+      .replace("repos/", "")
+      .replace("commits", "commit");
   }
 });
 
