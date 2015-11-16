7ce3c250831f5d355d56de0a3c2a6c04bd4cd73f
Richard Frank <richardfrank@me.com>
2015-11-16 17:27:33 -0600

    Moves logic for initial link creation to the Link model

--- ember-app/app/models/link.js
+++ ember-app/app/models/link.js
@@ -1,7 +1,27 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
 
-var Link = Ember.Object.extend({
+var Link = Ember.Object.extend({});
 
+Link.reopenClass({
+  build: function(name, repo, contraints){
+    var validate_url = `/api/${repo}/links/validate`;
+    var links_url = `/api/${repo}/links`;
+
+    var request = this.request;
+    var data = {link: name, labels: contraints};
+    return request(validate_url, data).then(()=> {
+      return request(links_url, data);
+    });
+  },
+  request: function(url, data){
+    return ajax({
+      url: url,
+      dataType: 'json',
+      type: 'POST',
+      data: data
+    })
+  }
 });
 
 export default Link;
