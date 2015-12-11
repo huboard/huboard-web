b2facf56eb7bdb18498ea3c7657fc77bb62b1b48
Richard Frank <richardfrank@me.com>
2015-12-11 12:40:07 -0600

    Enables updating a link with optional issue_filter params

--- ember-app/app/models/link.js
+++ ember-app/app/models/link.js
@@ -6,23 +6,23 @@ var Link = Ember.Object.extend({});
 Link.reopenClass({
   build: function(name, repo, issue_filter){
     var url = `/api/${repo}/links`;
-    return this.request(url, {link: name, labels: issue_filter});
+    return this.request(url, {link: name, labels: issue_filter, type: "POST"});
   },
   validate: function(name, repo){
     var url = `/api/${repo}/links/validate`;
-    return this.request(url, {link: name});
+    return this.request(url, {link: name, type: "POST"});
   },
   update: function(link, repo){
     var name = link.data.repo.full_name;
     var issue_filter = link.data.issue_filter;
     var url = `/api/${repo}/links/update`;
-    return this.request(url, {name: name, labels: issue_filter});
+    return this.request(url, {name: name, labels: issue_filter, type: "PUT"});
   },
   request: function(url, data){
     return ajax({
       url: url,
       dataType: 'json',
-      type: 'POST',
+      type: data.type,
       data: data
     })
   }
