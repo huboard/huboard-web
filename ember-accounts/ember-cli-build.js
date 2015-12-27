58ad75cd031e278494102e8d6c55212e8c474924
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-27 18:05:15 -0500

    Add in Bootstrap dependency.

--- ember-accounts/ember-cli-build.js
+++ ember-accounts/ember-cli-build.js
@@ -19,6 +19,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 
   return app.toTree();
 };
