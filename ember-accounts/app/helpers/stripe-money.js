2107616b7294b1dd10f88d3cc39160000d256e8f
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-01-04 20:40:41 -0600

    Update Stripe Money Helper.

--- ember-accounts/app/helpers/stripe-money.js
+++ ember-accounts/app/helpers/stripe-money.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
-export function stripeMoney(path) {
-  let value = Ember.get(this, path);
+export function stripeMoney(value) {
   return `$${parseFloat(value/100).toFixed(0)}`;
 }
 
