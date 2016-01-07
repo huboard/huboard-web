40d39c58fdcac2c0551c8f8cfc2d2907d9bd7b9d
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-01-06 19:17:23 -0600

    Add in loading overlay.

--- ember-accounts/ember-cli-build.js
+++ ember-accounts/ember-cli-build.js
@@ -28,7 +28,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  
+  app.import('bower_components/spin.js/spin.js')
 
   return app.toTree();
 };
