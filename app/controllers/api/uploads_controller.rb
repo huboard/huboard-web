6f835b4b6e5ec7f4a4712ac8e12a167aa7f4a458
Ryan Rauh <rauh.ryan@gmail.com>
2016-01-09 16:56:05 -0600

    add configuration for server-side encrypted s3 uploads

--- app/controllers/api/uploads_controller.rb
+++ app/controllers/api/uploads_controller.rb
@@ -3,19 +3,35 @@ module Api
 
     def asset_uploader
       not_found unless logged_in?
+      not_found unless ENV['AWS_ENABLED']
       uploader = AssetUploader.new
       uploader.will_include_content_type = true
       uploader.success_action_status = '201'
+
+      if ENV['AWS_S3_ENCRYPTED'] == 'true'
+        policy = uploader.policy do |conditions|
+          conditions << {"x-amz-server-side-encryption" => "AES256"}
+          conditions << {"x-amz-server-side-encryption-aws-kms-key-id" => ENV['AWS_KMS_KEY_ID']} if ENV['AWS_KMS_KEY_ID']
+          conditions << {'utf8' => '✓'}
+        end
+      else
+        policy = uploader.policy
+      end
+
+      uploader = {
+        key: uploader.key,
+        aws_access_key_id: uploader.aws_access_key_id,
+        acl: uploader.acl,
+        policy: policy,
+        signature: uploader.signature,
+        upload_url: uploader.direct_fog_url,
+        success_action_status: uploader.success_action_status
+      }
+
+      uploader.merge!(aws_kms_key_id: ENV['AWS_KMS_KEY_ID']) if ENV['AWS_KMS_KEY_ID']
+
       render json: {
-        uploader: {
-          key: uploader.key,
-          aws_access_key_id: uploader.aws_access_key_id,
-          acl: uploader.acl,
-          policy: uploader.policy,
-          signature: uploader.signature,
-          upload_url: uploader.direct_fog_url,
-          success_action_status: uploader.success_action_status
-        }
+        uploader: uploader
       }
     end
 
