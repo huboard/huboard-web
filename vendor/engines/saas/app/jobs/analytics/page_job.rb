400151c3ab350a25e47542386c20655ccd7466b2
Richard Frank <richardfrank@me.com>
2016-02-19 14:12:41 -0700

    Catches any potential nil current_user objects in PageJob's

--- vendor/engines/saas/app/jobs/analytics/page_job.rb
+++ vendor/engines/saas/app/jobs/analytics/page_job.rb
@@ -3,8 +3,9 @@ module Analytics
     action "page"
 
     def payload(params)
+      user = params['current_user'] ? params['current_user']['id'] || "Anonymous"
       {
-        user_id: params['current_user']['id'] || "Anonymous",
+        user_id: user,
         name: params['url'],
         properties: { url: params['url'] }
       }
