c792ce28fc87fb0139e6a79dde7bb15b88f5ad7e
Richard Frank <richardfrank@me.com>
2016-02-23 10:38:41 -0700

    Finishes V1 of the SendGrid mail adapter

--- vendor/engines/saas/lib/saas/mailers/sendgrid.rb
+++ vendor/engines/saas/lib/saas/mailers/sendgrid.rb
@@ -4,10 +4,16 @@ module Saas
 
       def initialize(config)
         @api_key = config[:api_key]
+        @client = SendGrid::Client.new(api_key)
       end
 
       def api_key
         @api_key || raise("#{self.class} missing api_key")
+      end
+
+      def send_mail(mail)
+        mail_object = SendGrid::Mail.new(mail)
+        @client.send(mail_object)
       end
     end
   end
--- vendor/engines/saas/spec/unit/lib/mailers/sendgrid_spec.rb
+++ vendor/engines/saas/spec/unit/lib/mailers/sendgrid_spec.rb
@@ -3,10 +3,19 @@ require 'spec_helper'
 describe Saas::Mailers::Sendgrid do
 
   let(:sut) { Saas::Mailers::Sendgrid }
-  let(:config) do
-    {
-      api_key: 'abc1234'
-    }
+  let(:config) {{ api_key: 'abc1234' }}
+
+  class MockClient
+    def initialize(config); end
+    def send(mail); end
+  end
+  class MockMail
+    def initialize(mail_config); end
+  end
+
+  before(:each) do
+    stub_const("SendGrid::Client", MockClient)
+    stub_const("SendGrid::Mail", MockMail)
   end
 
   context 'set configuration' do
@@ -15,12 +24,20 @@ describe Saas::Mailers::Sendgrid do
       adapter = sut.new(config)
       expect(adapter.api_key).to equal config[:api_key]
     end
+
+    it 'throws if missing api key' do
+      expect{ sut.new({}) }.to raise_error(RuntimeError, "Saas::Mailers::Sendgrid missing api_key")
+    end
   end
 
   context 'sending emails' do
 
-    it 'sends emails' do
+    let(:adapter) { sut.new(config) }
+    let(:mail){{ subject: "Hu", body: "Dat" }}
 
+    it 'sends emails' do
+      expect_any_instance_of(MockClient).to receive(:send)
+      adapter.send_mail(mail)
     end
   end
 end
