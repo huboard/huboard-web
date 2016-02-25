564b76d8111b34a8b11bf25e712464d6d2b0a604
Richard Frank <richardfrank@me.com>
2016-02-25 09:44:48 -0700

    Tweaks the CreateUserMail Job body

--- vendor/engines/saas/app/jobs/mailers/create_user_mail_job.rb
+++ vendor/engines/saas/app/jobs/mailers/create_user_mail_job.rb
@@ -1,9 +1,9 @@
 module Mailers
   class CreateUserMailJob < BaseMailerJob
     from 'no-reply@huboard.com'
-    subject 'Test'
-    text 'Test'
-    html '<p> Test </p>'
+    subject 'Thanks for using HuBoard!'
+    text ''
+    html ''
     template '9720d646-ccf3-4497-bca5-6ba6e92a7797'
   end
 end
