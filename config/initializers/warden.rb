9cf03c19444e08ae8048926cb6688c9f8747d354
Keith Dahlby <dahlbyk@huboard.com>
2016-02-26 00:39:27 -0500

    Adjust Warden redirect_uri for Heroku review apps
    Heroku sets APP_NAME and PARENT_APP_NAME environment variables.
    
    The latter needs to replace the former in the OAuth redirect hostname,
    and the former needs to be appended as a query parameter to use on return.
    
    Env:
    HEROKU_APP_NAME = hb-pr-123
    HEROKU_PARENT_APP_NAME = hb
    
    Input: https://hb-pr-123.herokuapp.com/
    Output: https://hb.herokuapp.com/?APP_NAME=hb-pr-123

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -21,6 +21,8 @@ end
 Warden::Manager.serialize_from_session { |key| Warden::GitHub::Verifier.load(key) }
 Warden::Manager.serialize_into_session { |user| Warden::GitHub::Verifier.dump(user) }
 
+require 'uri'
+
 module Warden
   module GitHub
     class Config
@@ -28,6 +30,15 @@ module Warden
 
       def normalized_uri(uri_or_path)
         uri = base_normalized_uri(uri_or_path)
+
+        app_name = ENV["HEROKU_APP_NAME"]
+        parent_app_name = ENV["HEROKU_PARENT_APP_NAME"]
+        puts "Warden Override: #{app_name} => #{parent_app_name}"
+        if parent_app_name && parent_app_name != app_name
+          uri.host.sub! app_name, parent_app_name
+          uri.query = URI.encode_www_form("APP_NAME" => app_name)
+        end
+
         puts "Warden Override: #{uri}"
         uri
       end
