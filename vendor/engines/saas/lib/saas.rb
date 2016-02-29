e7f603aff055c5602208b89d0bd97fd985a26928
Richard Frank <richardfrank@me.com>
2016-02-29 09:22:56 -0700

    Removes the current Mailer in preparation for replacing with ActionMailers

--- vendor/engines/saas/lib/saas.rb
+++ vendor/engines/saas/lib/saas.rb
@@ -1,8 +1,5 @@
 require "saas/before_action"
 require "saas/engine"
 
-require "saas/mailer"
-require "saas/mailers/sendgrid"
-
 module Saas
 end
--- vendor/engines/saas/lib/saas/mailer.rb
+++
@@ -1,9 +0,0 @@
-module Saas
-  class Mailer
-    attr_accessor :adapter
-
-    def send_mail(mail)
-      @adapter.send_mail(mail)
-    end
-  end
-end
