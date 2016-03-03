dbe2a605e2d4344d1473d37696f237f7872c154a
Richard Frank <richardfrank@me.com>
2016-03-03 15:32:24 -0700

    Track new trials based on repo owner

--- vendor/engines/saas/app/jobs/saas/trials_create_job.rb
+++ vendor/engines/saas/app/jobs/saas/trials_create_job.rb
@@ -2,8 +2,8 @@ module Saas
   class TrialsCreateJob < ActiveJob::Base
 
     def perform(params)
-      user = params['current_user']['login']
-      params['url'] = "/settings/#{user}/trialing"
+      owner = params['action_controller.params']['user']
+      params['url'] = "/settings/#{owner}/trialing"
       Analytics::PageJob.perform_later(params)
     end
   end
