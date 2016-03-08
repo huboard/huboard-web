9421a1e57abf6dd7645a56faa996fb3854e14843
Richard Frank <richardfrank@me.com>
2016-03-08 10:20:17 -0700

    Adds a TODO to fix this hack later

--- app/jobs/issue_event_job.rb
+++ app/jobs/issue_event_job.rb
@@ -19,6 +19,7 @@ class IssueEventJob < ActiveJob::Base
   end
 
   def self.build_meta(params)
+    #TODO Fix this hack on remapping params to HWIA
     params = HashWithIndifferentAccess.new(params)
     issue = HashWithIndifferentAccess.new(params['issue'])
     action = @_action.is_a?(String) ? @_action : @_action.call(params)
