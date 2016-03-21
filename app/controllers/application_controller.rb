c52cbe1aee4c92c0db46f83d7b0cf3ece12c5e43
Richard Frank <richardfrank@me.com>
2016-03-21 13:18:33 -0600

    Testing without the rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from ActionController::InvalidAuthenticityToken, :with => :csrf_failed
   rescue_from Ghee::Unauthorized, :with => :ghee_unauthorized
-  rescue_from Ghee::ServiceUnavailable, Ghee::BadGateway, :with => :ghee_unreachable
+  #rescue_from Ghee::ServiceUnavailable, Ghee::BadGateway, :with => :ghee_unreachable
 
   include ApplicationHelper
 
