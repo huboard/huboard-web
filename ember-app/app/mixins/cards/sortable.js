5417be302bb8979b105e4b73b8c78466b9e48867
Ryan Rauh <rauh.ryan@gmail.com>
2016-04-25 12:15:44 -0500

    converts the columns over to use sortablejs

--- ember-app/app/mixins/cards/sortable.js
+++ ember-app/app/mixins/cards/sortable.js
@@ -11,56 +11,61 @@ var SortableMixin = Ember.Mixin.create(CardMoveMixin, {
     var columns = this.get("columnComponents");
     this.$(".cards").each(function(){
       var sortable = new sortablejs(this,{
-        group: 'column'
+        group: 'column',
+        draggable: 'li.is-draggable',
+        onMove: (evt) => {
+          var from = $(evt.from),
+            to = $(evt.to);
+
+          from.removeClass('hovering');
+          to.addClass('hovering');
+        },
+        onStart: (evt) => {
+          delete cardMove.data;
+          cardMove.data = {};
+          cardMove.data.originIndex = evt.oldIndex;
+          
+          var column = cardMove.findColumn(evt.item, columns);
+          var card = cardMove.findCard(evt.item, column);
+          cardMove.data.card = card;
+          console.log(column, card);
+        },
+        onEnd: (evt) => {
+          var from = $(evt.from),
+            to = $(evt.to);
+
+          from.removeClass('hovering');
+          to.removeClass('hovering');
+        },
+        onSort: (evt) => {
+          if(evt.item.parentNode !== evt.to) { return; }
+          var column = cardMove.findColumn(evt.item, columns);
+          cardMove.data.column = column;
+
+          var index = evt.newIndex;
+          var column_changed = evt.from;
+          var mod = cardMove.indexModifier(index, column_changed);
+
+          var issues = column.get("sortedIssues");
+          var issue_above = cardMove.issueAbove(index, issues, mod);
+          var issue_below = cardMove.issueBelow(index, issues, mod);
+
+          var issue_order = cardMove.calculateIssueOrder(issue_above, issue_below);
+          var issue = cardMove.data.card.get("issue");
+
+          var cancelMove = function(){ 
+            debugger;
+            Ember.$(ui.sender).sortable("cancel");
+          };
+          var moveIssue = function(){
+            column.moveIssue(issue, issue_order, cancelMove);
+          };
+
+          _self.executeAfterBrowserPaint(moveIssue);
+        }
       });
     });
 
-    return;
-    this.$(".cards").sortable({
-      helper: function(ev,ui) {
-        cardMove.data = {};
-        cardMove.data.originIndex = ui.index();
-
-        var column = cardMove.findColumn(ui, columns);
-        var card = cardMove.findCard(ui, column);
-        cardMove.data.card = card;
-        return ui;
-      },
-      items: "li.is-draggable",
-      placeholder: "ui-sortable-placeholder",
-      connectWith: "ul.cards",
-      over: function(){
-        _self.set('isHovering', true);
-      },
-      out: function(){
-        _self.set('isHovering', false);
-      },
-      update: function(ev, ui){
-        if (this !== ui.item.parent()[0]){return ;}
-        var column = cardMove.findColumn(ui.item, columns);
-        cardMove.data.column = column;
-
-        var index = ui.item.index();
-        var column_changed = ui.sender;
-        var mod = cardMove.indexModifier(index, column_changed);
-
-        var issues = column.get("sortedIssues");
-        var issue_above = cardMove.issueAbove(index, issues, mod);
-        var issue_below = cardMove.issueBelow(index, issues, mod);
-
-        var issue_order = cardMove.calculateIssueOrder(issue_above, issue_below);
-        var issue = cardMove.data.card.get("issue");
-
-        var cancelMove = function(){ 
-          Ember.$(ui.sender).sortable("cancel");
-        };
-        var moveIssue = function(){
-          column.moveIssue(issue, issue_order, cancelMove);
-        };
-
-        _self.executeAfterBrowserPaint(moveIssue);
-      },
-    });
   }.on("didInsertElement"),
 
   executeAfterBrowserPaint: function(callback){
