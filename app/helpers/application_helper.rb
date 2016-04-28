5bb29d72506ff3b43bb46cced7ec4c9fe809f857
Ryan Rauh <rauh.ryan@gmail.com>
2016-04-28 10:58:31 -0500

    switch based on the queue adapter. It's possible to deploy HuBoard in production with sucker_punch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -69,10 +69,10 @@ module ApplicationHelper
     verb = action.present_tense
     constant = "Api::Issues#{verb.capitalize}IssueJob".constantize
 
-    if Rails.env.production? # sucker_punch doesn't support enqueue
-      constant.set(wait: 1.second).perform_later message
-    else
+    if Rails.configuration.active_job.queue_adapter == :sucker_punch # sucker_punch doesn't support enqueue
       constant.perform_later message
+    else
+      constant.set(wait: 1.second).perform_later message
     end
   end
 end
