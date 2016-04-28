f59ddeb86d57051ca1178a0b33c3f2a344f446c1
Keith Dahlby <dahlbyk@huboard.com>
2016-04-28 16:36:54 -0500

    Extract storage function

--- ember-app/app/models/settings.js
+++ ember-app/app/models/settings.js
@@ -26,47 +26,50 @@ function getStore(name){
     };
 }
 
-var name = 'localStorage';
-var store = getStore(name);
-var Settings = Ember.Object.extend({
-  init: function (){
-    this._super.apply(this, arguments);
-    this.set("data", this.loadData()[this.get('dataKey')] || {});
-  },
-  metaEnterEnabled: attr(true),
-  enterEnabled: attr(false),
-  showColumnCounts: attr(false),
-  data: {},
-  loadData: function () {
-    var storage = store.getItem(`${name}:${this.get("storageKey")}`);
-    return storage ? JSON.parse(storage) : {};
-  },
-  storageKey: Ember.computed.alias("repo.repo.full_name"),
-  dataKey: 'settings',
-  changed: 0,
-  saveKey: function(key, value) {
-    this.set("data." + key, value);
-    this.saveData();
-  },
-  saveData: function() {
+function storage(name) {
+  var store = getStore(name);
+  return Ember.Object.extend({
+    init: function (){
+      this._super.apply(this, arguments);
+      this.set("data", this.loadData()[this.get('dataKey')] || {});
+    },
+    metaEnterEnabled: attr(true),
+    enterEnabled: attr(false),
+    showColumnCounts: attr(false),
+    data: {},
+    loadData: function () {
+      var storage = store.getItem(`${name}:${this.get("storageKey")}`);
+      return storage ? JSON.parse(storage) : {};
+    },
+    storageKey: Ember.computed.alias("repo.repo.full_name"),
+    dataKey: 'settings',
+    changed: 0,
+    saveKey: function(key, value) {
+      this.set("data." + key, value);
+      this.saveData();
+    },
+    saveData: function() {
 
-    var data = this.loadData();
+      var data = this.loadData();
 
-    data[this.get('dataKey')] = this.get("data");
+      data[this.get('dataKey')] = this.get("data");
 
-    store.setItem(`${name}:${this.get("storageKey")}`, JSON.stringify(data));
-    this.incrementProperty('changed');
-  },
-  setUnknownProperty: function(key, value) {
-    Ember.defineProperty(this, key, attr(false));
-    this.set(key, value);
-  },
-  unknownProperty: function(key) {
-    return this.get("data." + key);
-  },
-  available: function(){
-    return !store.fake;
-  }.property("")
-});
+      store.setItem(`${name}:${this.get("storageKey")}`, JSON.stringify(data));
+      this.incrementProperty('changed');
+    },
+    setUnknownProperty: function(key, value) {
+      Ember.defineProperty(this, key, attr(false));
+      this.set(key, value);
+    },
+    unknownProperty: function(key) {
+      return this.get("data." + key);
+    },
+    available: function(){
+      return !store.fake;
+    }.property("")
+  });
+}
+
+var Settings = storage('localStorage');
 
 export default Settings;
