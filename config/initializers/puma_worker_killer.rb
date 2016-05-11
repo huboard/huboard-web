e53cc22b864558ce19a830fbae3f6fe6009d9aab
Ryan Rauh <rauh.ryan@gmail.com>
2016-05-11 09:19:46 -0500

    adds an env value to tweak max memory

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,5 +1,5 @@
 PumaWorkerKiller.config do |config|
-  config.ram = 1024
+  config.ram = ENV['DYNO_MAX_MEMORY'].nil? ? 512 : ENV['DYNO_MAX_MEMORY'].to_i
   config.percent_usage = 0.9
 end
 PumaWorkerKiller.enable_rolling_restart if ENV['DYNO'] 
