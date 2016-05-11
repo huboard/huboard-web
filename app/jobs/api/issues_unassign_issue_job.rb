7ce6330ab72671521810fda3712c34e31c76fb6e
Richard Frank <richardfrank@me.com>
2016-05-11 16:06:03 -0600

    Matches cache keys for Assigned and Unassigned jobs

--- app/jobs/api/issues_unassign_issue_job.rb
+++ app/jobs/api/issues_unassign_issue_job.rb
@@ -3,6 +3,10 @@ module Api
     include IsPublishable
     action 'unassigned'
     timestamp ->(params) { params[:issue]['updated_at'] }
+    cache_key ->(message) {
+      "assigned.#{message[:meta][:user]["login"]}.#{message[:meta][:identifier]}.#{message[:meta][:timestamp]}"
+    }
+
     def payload(params)
       {
         issue: params[:issue]
