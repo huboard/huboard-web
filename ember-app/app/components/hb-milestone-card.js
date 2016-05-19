724a28e4330f157790026a969fe465da1300660f
Ryan Rauh <rauh.ryan@gmail.com>
2016-05-19 17:54:34 -0500

    spikes out a simple column indicator

--- ember-app/app/components/hb-milestone-card.js
+++ ember-app/app/components/hb-milestone-card.js
@@ -1,7 +1,20 @@
 import HbCard from "../components/hb-task-card";
+import Ember from "ember";
+
+var get = Ember.get;
 
 var HbMilestoneCard = HbCard.extend({
-  classNames: ["card", "card--milestone"]
+  classNames: ["card", "card--milestone"],
+  columnIndicator: function(){
+    let currentState = get(this, 'issue.current_state');
+    return this.get('taskColumns').map((c) => {
+      return Ember.Object.create({
+        index: get(c, 'data.index'),
+        selected: get(c, 'data.index') == get(currentState, 'index'),
+        name: get(currentState, 'text')
+      })
+    });
+  }.property('issue.data.current_state', 'taskColumns')
 });
 
 export default HbMilestoneCard;
