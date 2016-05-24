f7cb9546c1457a2d97d333a261dffab02181744a
Ryan Rauh <rauh.ryan@gmail.com>
2016-05-24 15:00:58 -0500

    "fills" up the power meter for the column indicator

--- ember-app/app/components/hb-milestone-card.js
+++ ember-app/app/components/hb-milestone-card.js
@@ -1,19 +1,39 @@
 import HbCard from "../components/hb-task-card";
 import Ember from "ember";
 
-var get = Ember.get;
+var { get, set } = Ember;
+
+var takeWhile = function(list, callback, context) {
+  var xs = [];
+  _.any(list, function(item, index, list){
+    var result = callback.call(context, item, index, list);
+    if(result){
+      xs.push(item);
+      return false;
+    } else {
+      return true;
+    }
+  });
+  return xs;
+};
 
 var HbMilestoneCard = HbCard.extend({
   classNames: ["card", "card--milestone"],
   columnIndicator: function(){
     let currentState = get(this, 'issue.current_state');
-    return this.get('taskColumns').map((c) => {
+    var columns = this.get('taskColumns').map((c) => {
       return Ember.Object.create({
         index: get(c, 'data.index'),
         selected: get(c, 'data.index') == get(currentState, 'index'),
         name: get(currentState, 'text')
       })
     });
+
+    takeWhile(columns, (x) => {return !get(x, 'selected');})
+      .forEach((x) => { set(x, 'selected', true); })
+
+    return columns;
+
   }.property('issue.data.current_state', 'taskColumns')
 });
 
