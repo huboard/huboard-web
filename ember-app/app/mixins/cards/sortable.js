b771578e3fd73160cd69f698ba81f13f28189b78
Ryan Rauh <rauh.ryan@gmail.com>
2016-05-24 15:31:12 -0500

    cleans up the sortable branching logic

--- ember-app/app/mixins/cards/sortable.js
+++ ember-app/app/mixins/cards/sortable.js
@@ -10,120 +10,66 @@ var SortableMixin = Ember.Mixin.create(CardMoveMixin, {
     var _self = this;
     var cardMove = this.cardMover;
     var columns = this.get("columnComponents");
-    if(!_self.get('newSortable')) {
-      this.$(".cards").sortable({
-        helper: function(ev,ui) {
-          cardMove.data = {};
-          cardMove.data.originIndex = ui.index();
+    var newSortable = _self.get('newSortable');
+    var sortableFx = newSortable ? "superSortable" : "sortable";
+    this.$(".cards")[sortableFx]({
+      appendTo: newSortable ? document.body : "parent",
+      helper: function(ev,ui) {
+        cardMove.data = {};
+        cardMove.data.originIndex = ui.index();
 
-          var column = cardMove.findColumn(ui, columns);
-          var card = cardMove.findCard(ui, column);
-          cardMove.data.card = card;
-          return ui.clone();
-        },
-        start: function(ev, ui){
-          ui.placeholder.height(ui.item.outerHeight());
-        },
-        items: ".is-draggable",
-        placeholder: "ui-sortable-placeholder",
-        connectWith: ".cards",
-        over: function(){
-          _self.set('isHovering', true);
-        },
-        out: function(){
-          _self.set('isHovering', false);
-        },
-        update: function(ev, ui){
-          if (this !== ui.item.parent()[0]){return ;}
+        var column = cardMove.findColumn(ui, columns);
+        var card = cardMove.findCard(ui, column);
+        cardMove.data.card = card;
+        return ui.clone();
+      },
+      start: function(ev, ui){
+        ui.placeholder.height(ui.item.outerHeight());
+      },
+      items: ".is-draggable",
+      placeholder: "ui-sortable-placeholder",
+      connectWith: ".cards",
+      over: function(){
+        _self.set('isHovering', true);
+      },
+      out: function(){
+        _self.set('isHovering', false);
+      },
+      update: function(ev, ui){
+        if (this !== ui.item.parent()[0]){return ;}
 
-          var column = cardMove.findColumn(ui.item, columns);
-          cardMove.data.column = column;
+        var column = cardMove.findColumn(ui.item, columns);
+        cardMove.data.column = column;
 
-          var index = ui.item.index();
-          var column_changed = ui.sender;
-          var mod = cardMove.indexModifier(index, column_changed);
+        var index = ui.item.index();
+        var column_changed = ui.sender;
+        var mod = cardMove.indexModifier(index, column_changed);
 
-          var issues = column.get("sortedIssues");
-          var issue_above = cardMove.issueAbove(index, issues, mod);
-          var issue_below = cardMove.issueBelow(index, issues, mod);
+        var issues = column.get("sortedIssues");
+        var issue_above = cardMove.issueAbove(index, issues, mod);
+        var issue_below = cardMove.issueBelow(index, issues, mod);
 
-          var issue_order = cardMove.calculateIssueOrder(issue_above, issue_below);
-          var issue = cardMove.data.card.get("issue");
+        var issue_order = cardMove.calculateIssueOrder(issue_above, issue_below);
+        var issue = cardMove.data.card.get("issue");
 
-          var cancelMove = function(){ 
-            Ember.$(ui.sender).sortable("cancel");
-          };
-          var moveIssue = function(){
-            column.moveIssue(issue, issue_order, cancelMove);
-          };
+        var cancelMove = function(){ 
+          Ember.$(ui.sender).sortable("cancel");
+        };
+        var moveIssue = function(){
+          column.moveIssue(issue, issue_order, cancelMove);
+        };
 
-          _self.executeAfterBrowserPaint(moveIssue);
-        },
-      });
+        _self.executeAfterBrowserPaint(moveIssue);
+      },
+    });
+   }.on("didInsertElement"),
 
-    } else {
-      
-      this.$(".cards").superSortable({
-        appendTo: document.body,
-        helper: function(ev,ui) {
-          cardMove.data = {};
-          cardMove.data.originIndex = ui.index();
-
-          var column = cardMove.findColumn(ui, columns);
-          var card = cardMove.findCard(ui, column);
-          cardMove.data.card = card;
-          return ui.clone();
-        },
-        start: function(ev, ui){
-          ui.placeholder.height(ui.item.outerHeight());
-        },
-        scroll:false,
-        items: ".is-draggable",
-        placeholder: "ui-sortable-placeholder",
-        connectWith: ".cards",
-        over: function(){
-          _self.set('isHovering', true);
-        },
-        out: function(){
-          _self.set('isHovering', false);
-        },
-        update: function(ev, ui){
-          if (this !== ui.item.parent()[0]){return ;}
-
-          var column = cardMove.findColumn(ui.item, columns);
-          cardMove.data.column = column;
-
-          var index = ui.item.index();
-          var column_changed = ui.sender;
-          var mod = cardMove.indexModifier(index, column_changed);
-
-          var issues = column.get("sortedIssues");
-          var issue_above = cardMove.issueAbove(index, issues, mod);
-          var issue_below = cardMove.issueBelow(index, issues, mod);
-
-          var issue_order = cardMove.calculateIssueOrder(issue_above, issue_below);
-          var issue = cardMove.data.card.get("issue");
-
-          var cancelMove = function(){ 
-            Ember.$(ui.sender).superSortable("cancel");
-          };
-          var moveIssue = function(){
-            column.moveIssue(issue, issue_order, cancelMove);
-          };
-
-          _self.executeAfterBrowserPaint(moveIssue);
-        },
-      });
-    }
-
-  }.on("didInsertElement"),
-
-  executeAfterBrowserPaint: function(callback){
-    if(window.requestAnimationFrame){
-      return window.requestAnimationFrame(callback);
-    }
-    callback();
-  }
+   executeAfterBrowserPaint: function(callback){
+     if(window.requestAnimationFrame){
+       return window.requestAnimationFrame(callback);
+     }
+     callback();
+   }
 });
 
 export default SortableMixin;
