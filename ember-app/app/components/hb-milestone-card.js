473151aa19e521c7fd11062bc6572dd70fbb32bb
Ryan Rauh <rauh.ryan@gmail.com>
2016-05-26 12:28:47 -0500

    code clean up and jshint fixes

--- ember-app/app/components/hb-milestone-card.js
+++ ember-app/app/components/hb-milestone-card.js
@@ -24,13 +24,17 @@ var HbMilestoneCard = HbCard.extend({
     var columns = this.get('taskColumns').map((c) => {
       return Ember.Object.create({
         index: get(c, 'data.index'),
+        /* jshint ignore:start */
+        // For now, I'm ok with matching "1" to 1, need to look this over and make sure
+        // columns are true index based
         selected: get(c, 'data.index') == get(currentState, 'index'),
+        /* jshint ignore:end */
         name: get(currentState, 'text')
-      })
+      });
     });
 
-    takeWhile(columns, (x) => {return !get(x, 'selected');})
-      .forEach((x) => { set(x, 'selected', true); })
+    takeWhile(columns, (x) => {return !get(x, 'selected');}, this)
+      .forEach((x) => { set(x, 'selected', true); });
 
     return columns;
   }.property('issue.data.current_state', 'taskColumns')
