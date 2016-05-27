76f3c1082cf11ebd94ec8306594f908c8efa618a
Ryan Rauh <rauh.ryan@gmail.com>
2016-05-27 18:20:27 -0500

    removes an old helper

--- ember-app/app/helpers/contrast-color.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function contrastColor(params) {
-  let value = params[0];
-  return Ember.$.Color('#' + value).contrastColor();
-}
-
-export default Ember.Helper.helper(contrastColor);
