b10d9530a634d0f831c81acdcb4055bfb86bc2c9
Ryan Rauh <rauh.ryan@gmail.com>
2016-06-06 16:01:50 -0500

    adds the deprecations package to silence all the deprecation warnings

---
+++ ember-app/config/deprecation-workflow.js
@@ -0,0 +1,15 @@
+window.deprecationWorkflow = window.deprecationWorkflow || {};
+window.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchMessage: "Ember.LinkView is deprecated. Please use Ember.LinkComponent." },
+    { handler: "silence", matchMessage: "Ember.keys is deprecated in favor of Object.keys" },
+    { handler: "silence", matchId: "ember-htmlbars.make-bound-helper" },
+    { handler: "silence", matchId: "ember-htmlbars.register-helper" },
+    { handler: "silence", matchId: "ember-views.view-deprecated" },
+    { handler: "silence", matchMessage: "Controller#needs is deprecated, please use Ember.inject.controller() instead" },
+    { handler: "silence", matchMessage: "Using the same function as getter and setter is deprecated." },
+    { handler: "silence", matchId: "ember-metal.@each-dependent-key-leaf" },
+    { handler: "silence", matchMessage: "Ember.create is deprecated in favor of Object.create" },
+    { handler: "silence", matchId: "ember-views.dispatching-modify-property" }
+  ]
+};
