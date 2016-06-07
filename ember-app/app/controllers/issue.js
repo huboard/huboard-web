fd76b0b378594d75321e4ef421f8971296f0725c
Ryan Rauh <rauh.ryan@gmail.com>
2016-06-06 19:17:31 -0500

    shared getter and setter deprecations

--- ember-app/app/controllers/issue.js
+++ ember-app/app/controllers/issue.js
@@ -15,8 +15,11 @@ var IssueController = Ember.Controller.extend(
     return this.get("model.repo.isCollaborator");
   }.property("model.repo.isCollaborator"),
   columns: Ember.computed.alias("application.model.board.columns"),
-  isReady: function(key, value){
-    if(value !== undefined) {
+  isReady: Ember.computed("model.customState", "model.data._data.custom_state", {
+    get: function() {
+      return this.get("model.customState") === "ready";
+    },
+    set: function(key, value){
       if(value) {
         this.set("model.customState", "ready");
         return true; 
@@ -24,24 +27,22 @@ var IssueController = Ember.Controller.extend(
         this.set("model.customState", "");
         return false;
       }
-    } else {
-      return this.get("model.customState") === "ready";
     }
-  }.property("model.customState", "model.data._data.custom_state"),
-  isBlocked: function(key, value){
-    if(value !== undefined) {
+  }),
+  isBlocked: Ember.computed("model.customState", "model.data._data.custom_state", {
+    get: function() {
+      return this.get("model.customState") === "blocked";
+    },
+    set: function(key, value){
       if(value) {
         this.set("model.customState", "blocked");
-        return true;
+        return true; 
       } else {
         this.set("model.customState", "");
         return false;
       }
-      return;
-    } else {
-      return this.get("model.customState") === "blocked";
     }
-  }.property("model.customState", "model.data._data.custom_state"),
+  }),
   isClosed: function(){
     return this.get("model.data.state") === "closed";
   }.property("model.data.state"),
--- ember-app/config/deprecation-workflow.js
+++ ember-app/config/deprecation-workflow.js
@@ -7,7 +7,7 @@ window.deprecationWorkflow.config = {
     { handler: "throw", matchId: "ember-htmlbars.register-helper" },
     { handler: "throw", matchId: "ember-views.view-deprecated" },
     { handler: "throw", matchMessage: "Controller#needs is deprecated, please use Ember.inject.controller() instead" },
-    { handler: "silence", matchMessage: "Using the same function as getter and setter is deprecated." },
+    { handler: "throw", matchMessage: "Using the same function as getter and setter is deprecated." },
     { handler: "silence", matchId: "ember-metal.@each-dependent-key-leaf" },
     { handler: "silence", matchMessage: "Ember.create is deprecated in favor of Object.create" },
     { handler: "silence", matchId: "ember-views.dispatching-modify-property" }
