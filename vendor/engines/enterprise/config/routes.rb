874c611dfc8b99bdf0d166a0f59cba2cbadbac9e
Ryan Rauh <rauh.ryan@gmail.com>
2016-06-10 13:59:58 -0500

    renders the login page in enterprise

--- vendor/engines/enterprise/config/routes.rb
+++ vendor/engines/enterprise/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do 
+  get 'login' => "login#index", as: "enterprise_login"
   match 'unauthenticated_enterprise' => 'enterprise/errors#unauthenticated_enterprise', via: :all
   get '/settings/profile', to: redirect('/')
   mount Enterprise::Engine => "/_enterprise"
