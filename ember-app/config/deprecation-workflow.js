7579d153fb9e4841070403ddeb7b4a28c8bc9a1f
Ryan Rauh <rauh.ryan@gmail.com>
2016-06-13 15:58:57 -0500

    re-ran flush and only came out with Ember.keys

--- ember-app/config/deprecation-workflow.js
+++ ember-app/config/deprecation-workflow.js
@@ -1,15 +1,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchMessage: "Ember.LinkView is deprecated. Please use Ember.LinkComponent." },
-    { handler: "silence", matchMessage: "Ember.keys is deprecated in favor of Object.keys" },
-    { handler: "throw", matchId: "ember-htmlbars.make-bound-helper" },
-    { handler: "throw", matchId: "ember-htmlbars.register-helper" },
-    { handler: "throw", matchId: "ember-views.view-deprecated" },
-    { handler: "throw", matchMessage: "Controller#needs is deprecated, please use Ember.inject.controller() instead" },
-    { handler: "throw", matchMessage: "Using the same function as getter and setter is deprecated." },
-    { handler: "throw", matchId: "ember-metal.@each-dependent-key-leaf" },
-    { handler: "throw", matchMessage: "Ember.create is deprecated in favor of Object.create" },
-    { handler: "throw", matchId: "ember-views.dispatching-modify-property" }
+    { handler: "silence", matchMessage: "Ember.keys is deprecated in favor of Object.keys" }
   ]
 };
