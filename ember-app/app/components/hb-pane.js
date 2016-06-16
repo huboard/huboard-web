d9d5b05be8b58cf3e0f15220e4e4aa680ae66b8d
Ryan Rauh <rauh.ryan@gmail.com>
2016-06-16 09:55:42 -0500

    fixes a jshint error

--- ember-app/app/components/hb-pane.js
+++ ember-app/app/components/hb-pane.js
@@ -4,7 +4,7 @@ var HbPaneComponent = Ember.Component.extend({
     classNameBindings: ["selected:active"],
     classNames: ["tab-pane"],
     addPane: function(){
-      Ember.run.scheduleOnce('afterRender', this, () => { this.get('parentView').addPane(this); })
+      Ember.run.scheduleOnce('afterRender', this, () => { this.get('parentView').addPane(this); });
     }.on('init'),
     selected: function() {
       return this.get('parentView.selected') === this;
