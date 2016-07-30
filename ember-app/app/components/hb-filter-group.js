abf5f8be01202341327ed9adf05df01ded1f54d0
Richard Frank <richardfrank@me.com>
2016-07-30 15:25:34 -0600

    Wires up localStorage to filter groups

--- ember-app/app/components/hb-filter-group.js
+++ ember-app/app/components/hb-filter-group.js
@@ -4,11 +4,16 @@ var HbFilterGroupComponent = Ember.Component.extend({
   tagName: ['ul'],
   classNames: ['filter-group'],
   classNameBindings: ['isCollapsed:collapsed'],
-  isCollapsed: false,
+  setCollapsedKey: function(){
+    if(this.get(`settings.filters${this.get('name')}Collapsed`)){
+      this.set(`settings.filters${this.get('name')}Collapsed`, false);
+    }
+  }.on('init'),
+  isCollapsed: Ember.computer.alias(`settings.filters${this.get('name')}Collapsed`),
   click: function(el){
     if(this.$(el.target).is('.filter-header') || this.$(el.target).is('.ui-icon')){
-      this.toggleProperty('isCollapsed');
-      //save to LS
+      this.toggleProperty(`settings.filters${this.get('name')}Collapsed`);
+      this.notifyPropertyChange('isCollapsed');
     }
   }
 });
