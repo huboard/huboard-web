4c355ed530d1b8fd298037110bb922ff893ec83d
Ryan Rauh <rauh.ryan@gmail.com>
2016-10-06 14:24:59 -0500

    duck punch the router to fix a bug in emberjs
    Relates to 
     - emberjs/ember.js#13981 
     - emberjs/ember.js#13771

---
+++ ember-app/app/initializers/punch-router.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+
+function addQueryParamsObservers(controller, propNames) {
+  propNames.forEach(function(prop) {
+    controller.addObserver(prop + '.[]', controller, controller._qpChanged);
+  });
+}
+
+let get = Ember.get;
+
+export function initialize(container, application){
+  Ember.Route.reopen({
+    setup(context, transition) {
+      var controller;
+
+      var controllerName = this.controllerName || this.routeName;
+      var definedController = this.controllerFor(controllerName, true);
+
+      if (!definedController) {
+        controller =  this.generateController(controllerName, context);
+      } else {
+        controller = definedController;
+      }
+
+
+      // Assign the route's controller so that it can more easily be
+      // referenced in action handlers. Side effects. Side effects everywhere.
+      if (!this.controller) {
+        var propNames = get(this, '_qp.propertyNames');
+        addQueryParamsObservers(controller, propNames);
+        this.controller = controller;
+      }
+
+      let queryParams = get(this, '_qp');
+
+      let states = queryParams.states;
+
+      controller._qpDelegate = states.allowOverrides;
+
+      return this._super(...arguments);
+    }
+  });
+}
+
+export default {
+  name: 'punch-router',
+  initialize: initialize
+};
+
