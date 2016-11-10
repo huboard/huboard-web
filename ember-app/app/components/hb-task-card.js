148ca5b0ca74658e3c53cc3c061d41c7431586bf
Ryan Rauh <rauh.ryan@gmail.com>
2016-11-10 14:53:49 -0600

    moves the display toggle to a checkbox in settings

--- ember-app/app/components/hb-task-card.js
+++ ember-app/app/components/hb-task-card.js
@@ -11,7 +11,7 @@ var HbCardComponent = Ember.Component.extend(
   Messaging, IssueFiltersMixin, MemberDragAndDropMixin, {
     attributeBindings: ['style'],
     classNames: ["card"],
-    classNameBindings: ["isFiltered","isDraggable:is-draggable", "isClosable:closable", "issue.linkedColor:border", "stateClass", "taskCard:task-card"],
+    classNameBindings: ["isFiltered","isDraggable:is-draggable", "isClosable:closable", "issue.linkedColor:border", "stateClass", "taskCard:task-card", "global.displaySetting:board-heavy:board-light"],
     taskCard: true,
     filters: Ember.inject.service(),
     repoName: Ember.computed.alias("issue.repoName"),
