60db5edb9f374a8f3ed4d738b439632d02cb597d
Richard Frank <richardfrank@me.com>
2017-01-31 15:33:30 -0500

    Adds rough script for pushing to cdn, configures rails to look for CDN_HOST

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['CDN_HOST'] if ENV['CDN_HOST'].present?
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- lib/tasks/push-to-cdn.rake
+++ lib/tasks/push-to-cdn.rake
@@ -1,6 +1,8 @@
 namespace "cdn" do
   task push: :environment do
-    files = Dir.glob "#{Rails.root}/public/assets/**/*"
-    $stdout.puts files
+    assets_dir = "#{Rails.root}/public/assets"
+    `gsutil -m rm gs://huboard-web/staging/assets/**`
+    `gsutil -m cp -r #{assets_dir} gs://huboard-web/staging`
+    `gsutil -m acl -R ch -u AllUsers:R gs://huboard-web/staging/assets`
   end
 end
